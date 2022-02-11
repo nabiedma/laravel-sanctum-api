4d62dcfdedcef186b18a1727c2fdad3e6e351d70
nabiedma <nabiedma@gmail.com>
2022-02-11 19:17:31 -0300

    added routes for all endpoints

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,9 @@ use App\Models\Product;
 // Register New User
 Route::post('/register', [AuthController::class, 'register']);
 
+// Login User
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
@@ -34,9 +37,12 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-
+// Token: 2|fQ7eogHeLoHDUDXyRNIPE696Z1QrKEdVGKGMZVmw
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
